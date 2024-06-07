45f49d7db13d178f512dadaf2e681a76d4cd977a
unknown <jayumanga>
2024-06-07 14:56:11 +0800

    remove

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,21 +14,6 @@
     <!-- /.content-header -->
 
     <!-- Main content -->
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <p class="card-text">
-                                {{ __('You are logged in!') }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
+   
     <!-- /.content -->
 @endsection
